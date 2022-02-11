dce81aeff2dda1db92b8706bd0de375f4b1c3a9a
Pablo Moreno <pablo.a.moreno@gmail.com>
2022-02-11 18:28:12 +0000

    Atlas web sc cli

---
+++ recipes/atlas-web-sc-cli/build.sh
@@ -0,0 +1,11 @@
+mkdir -p $PREFIX/bin
+CLI_DIR=$PREFIX/share/atlas-cli
+mkdir -p $CLI_DIR
+
+# compile JAR
+./gradlew --no-daemon :cli:bootJar
+
+cp cli/build/libs/atlas-cli-sc.jar $CLI_DIR
+rm -rf cli
+chmod a+x $RECIPE_DIR/atlas-cli-sc.sh
+cp $RECIPE_DIR/atlas-cli-sc.sh $PREFIX/bin/
